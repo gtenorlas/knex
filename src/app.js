901a5e631f24f72a21240250b7ccef7ade62f175
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-17 23:15:38 +0000

    Import userRouter and blogRouter in app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");   
+const userRouter = require("./routes/users");
+const blogRouter = require("./routes/blog");
 
 const app = express();
 
@@ -7,3 +9,6 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use('/users', userRouter);
+app.use('/blog', blogRouter);\
